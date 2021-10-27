6529492c021d1a33369b54d8bd8dc2d684f8cc54
Andrii <kotsupyr.a@gmail.com>
2021-10-27 10:14:49 +0300

    add card controller

---
+++ controllers/card.controller.js
@@ -0,0 +1,47 @@
+const { Card } = require("../models");
+
+module.exports = {
+  addCard: async function (req, res) {
+    try {
+      const { title } = req.body;
+      const card = await Card.create({ title });
+      res.status(201).json(card);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+
+  getAllCards: async function (req, res) {
+    try {
+      const cards = await Card.findAll();
+      if (cards.length === 0) {
+        res.json({ message: "There are no cards!" });
+      } else {
+        res.status(200).json(cards);
+      }
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+
+  removeCard: async function (req, res) {
+    try {
+      const { id } = req.query;
+      const deletedCard = await Card.destroy({ where: { id } });
+      res.status(200).json(deletedCard);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+
+  updateCard: async function (req, res) {
+    try {
+      const { id } = req.query;
+      const { title } = req.body;
+      const updatedCard = await Card.update({ title }, { where: { id } });
+      res.status(201).json(updatedCard);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+};
