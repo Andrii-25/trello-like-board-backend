ed0f65905614df08b5ccdf84340ff7f0a27a87ed
Andrii <kotsupyr.a@gmail.com>
2021-10-27 09:52:59 +0300

    setup database

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const env = process.env.NODE_ENV || "development";
+const Sequelize = require("sequelize");
+const List = require("./list");
+const Card = require("./card");
+const config = require("../config/config")[env];
+
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+db.List = List(sequelize, Sequelize.DataTypes);
+db.Card = Card(sequelize, Sequelize.DataTypes);
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
